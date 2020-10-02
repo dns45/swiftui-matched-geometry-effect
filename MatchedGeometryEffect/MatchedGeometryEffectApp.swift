c7854a715012b2addc7e2d63fe12bdd982098ada
dns45 <dns45.one@gmail.com>
2020-10-02 23:57:40 +0200

    Generate SwiftUI project

---
+++ MatchedGeometryEffect/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  MatchedGeometryEffect
+//
+//  Created by Teply, Daniel on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
