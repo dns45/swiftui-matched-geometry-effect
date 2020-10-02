8e3b863193ee909cbd66eb87b76b3870522ab78b
dns45 <dns45.one@gmail.com>
2020-10-03 00:16:25 +0200

    Update home screen (navigation view)

---
+++ MatchedGeometryEffect/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  MatchedGeometryEffect
+//
+//  Created by Teply, Daniel on 02/10/2020.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        NavigationView {
+            Text("TBD")
+                .navigationBarTitle("SwiftUI Animations 🎩")
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
